2c7f9aa4f6629395cfd9952f32083d9a1516d227
Anand Doshi <anand@erpnext.com>
2015-02-18 12:21:15 +0530

    [minor] disable all applications only for demo user

--- erpnext_demo/public/js/desktop.js
+++ erpnext_demo/public/js/desktop.js
@@ -1,5 +1,5 @@
 $(document).on("desktop-render", function() {
-	if (user !== "Administrator") {
+	if (user === "demo@frappecloud.com") {
 		frappe.desktop.all_applications.show = function() {
 			msgprint(__("This feature is disabled for ERPNext Demo."));
 		}
